31581cd029a716a065d1a13f12fb1e20f60fd528
SigveVermandere <sigvevermandere@gmail.com>
2019-10-24 14:46:26 +0200

    tile polygon extraction

--- src/Polygon/Utils.ts
+++ src/Polygon/Utils.ts
@@ -23,20 +23,20 @@ export default class PolygonUtils {
 
     constructor(input: Array<{lat: number, lng: number}>) {
         this.polygon = this.createPolygon(input);
-        console.log(this.polygon);
+        // console.log(this.polygon);
         this.bbox = this.calculateBBox(input);
         this.tBoundingBox = this.calculateTilesWithinBBox();
     }
 
     public createPolygon(coords: Array<{lat: number, lng: number}>): Feature<Polygon> {
         const turfCoords: Position[] = [];
-        console.log(coords);
+        // console.log(coords);
         coords.forEach( (coord) => {
-            console.log(coord);
+            // console.log(coord);
             turfCoords.push([coord.lat, coord.lng]);
         });
         turfCoords.push([coords[0].lat, coords[0].lng]);
-        console.log("turfcoords: " + turfCoords);
+        // console.log("turfcoords: " + turfCoords);
         return polygon([turfCoords]);
     }
 
@@ -51,7 +51,7 @@ export default class PolygonUtils {
         let maxLon = - Number.MAX_SAFE_INTEGER;
 
         coords.forEach( (coord) => {
-            console.log(coord);
+            // console.log(coord);
             if (coord.lat < minLat) {
                 minLat = coord.lat;
             }
@@ -65,7 +65,7 @@ export default class PolygonUtils {
                 maxLon = coord.lng;
             }
         });
-        console.log({minLat, minLon, maxLat, maxLon});
+        // console.log({minLat, minLon, maxLat, maxLon});
         return {minLat, minLon, maxLat, maxLon};
     }
 
@@ -78,12 +78,14 @@ export default class PolygonUtils {
 
     public calculateTilesWithinBBox(): ITilesBoundingBox {
         const minTile = this.getTile(this.bbox.minLat, this.bbox.minLon, Config.context.zoom);
+        // console.log(minTile);
         const maxTile = this.getTile(this.bbox.maxLat, this.bbox.maxLon, Config.context.zoom);
+        // console.log(maxTile);
 
         minTile.xTile += 1;
-        minTile.yTile += 1;
+        minTile.yTile -= 1;
         maxTile.xTile -= 1;
-        maxTile.yTile -= 1;
+        maxTile.yTile += 1;
         console.log({minTile, maxTile});
         return {minTile, maxTile};
     }
@@ -95,11 +97,14 @@ export default class PolygonUtils {
             {tile: Feature<Polygon>, row: number, col: number} | undefined
             ) =
             this.findFirstTileInGrid(tilePolys);
+        console.log(tilePolys);
         if (typeof firstPolyWithIndex === "undefined") {
             return;
         }
-        return this.calculateTilePolygonRecursive(
+        tilePolys[firstPolyWithIndex.row][firstPolyWithIndex.col] = undefined;
+        const resultPolygon = this.calculateTilePolygonRecursive(
             firstPolyWithIndex.tile, firstPolyWithIndex.row, firstPolyWithIndex.col, tilePolys);
+        return this.simplifyPolygon(resultPolygon);
     }
 
     public findFirstTileInGrid(tilePolys: Array<Array<Feature<Polygon> | undefined>>):
@@ -124,20 +129,12 @@ export default class PolygonUtils {
     ): Feature<Polygon> {
         // tile to the left
         currTile = this.addTileToUnion(currTile, row, col - 1, tilePolys);
-        // tile to the upper left
-        currTile = this.addTileToUnion(currTile, row + 1, col - 1, tilePolys);
-        // upper tille
-        currTile = this.addTileToUnion(currTile, row + 1, col, tilePolys);
-        // upper right tile
-        currTile = this.addTileToUnion(currTile, row + 1, col + 1, tilePolys);
+        // upper tile
+        currTile = this.addTileToUnion(currTile, row - 1, col, tilePolys);
         // right tile
         currTile = this.addTileToUnion(currTile, row, col + 1, tilePolys);
-        // lower right tile
-        currTile = this.addTileToUnion(currTile, row - 1, col + 1, tilePolys);
         // lower tile
-        currTile = this.addTileToUnion(currTile, row - 1, col, tilePolys);
-        // lower left tile
-        currTile = this.addTileToUnion(currTile, row - 1, col - 1, tilePolys);
+        currTile = this.addTileToUnion(currTile, row + 1, col, tilePolys);
         return currTile;
     }
 
@@ -147,35 +144,66 @@ export default class PolygonUtils {
         col: number,
         tilePolys: Array<Array<Feature<Polygon> | undefined>>,
     ): Feature<Polygon> {
-        if (row >= 0 && col >= 0 && typeof tilePolys[row][col] !== "undefined") {
+        if (row >= 0 && row < tilePolys.length &&
+            col >= 0 && col < tilePolys[0].length && typeof tilePolys[row][col] !== "undefined") {
+            console.log(tilePolys[row][col]);
+            console.log(currTile);
             currTile =
-                union(currTile, tilePolys[row][col - 1] as Feature<Polygon>) as
+                union(currTile, tilePolys[row][col] as Feature<Polygon>) as
                     Feature<Polygon>;
-            tilePolys[row][col - 1] = undefined;
-            this.calculateTilePolygonRecursive(currTile, row, col - 1, tilePolys);
+            tilePolys[row][col] = undefined;
+            currTile = this.calculateTilePolygonRecursive(currTile, row, col, tilePolys);
         }
         return currTile;
     }
 
     public calculateTilesWithinPolygon(): Array<Array<Feature<Polygon> | undefined>> {
-        const rows = this.tBoundingBox.maxTile.yTile - this.tBoundingBox.minTile.yTile;
-        const cols = this.tBoundingBox.maxTile.xTile - this.tBoundingBox.minTile.xTile;
-        // TODO: check if this works
-        const tilePolys: Array<Array<Feature<Polygon> | undefined>> =
-            new Array<Array<Feature<Polygon> | undefined>>(rows).map(
-                () => new Array<Feature<Polygon> | undefined>(cols).fill(undefined));
+        const rows = this.tBoundingBox.minTile.yTile - this.tBoundingBox.maxTile.yTile + 1;
+        const cols = this.tBoundingBox.maxTile.xTile - this.tBoundingBox.minTile.xTile + 1;
+        const tilePolys: Array<Array<Feature<Polygon> | undefined>> = [];
+        for (let i = 0; i < rows; i++) {
+            tilePolys[i] = [];
+            for (let j = 0; j < cols; j++) {
+                tilePolys[i][j] = undefined;
+            }
+        }
+        const offsetXtile = this.tBoundingBox.minTile.xTile;
+        const offsetYtile = this.tBoundingBox.maxTile.yTile;
         for (let currX = this.tBoundingBox.minTile.xTile; currX <= this.tBoundingBox.maxTile.xTile; currX++) {
-            for (let currY = this.tBoundingBox.minTile.yTile; currY <= this.tBoundingBox.maxTile.yTile; currY++) {
+            for (let currY = this.tBoundingBox.maxTile.yTile; currY <= this.tBoundingBox.minTile.yTile; currY++) {
                 const bb = globalMercator.googleToBBox([currX, currY, Config.context.zoom]);
+                // console.log(bb);
                 const bboxPoly: Feature<Polygon> = bboxPolygon([bb[1], bb[0], bb[3], bb[2]]);
                 if (booleanContains(this.polygon, bboxPoly)) {
-                    tilePolys[rows - currY][cols - currX] = bboxPoly;
+                    // console.log("row: " + (currY - offsetYtile).toString());
+                    // console.log("col: " + (currX - offsetXtile).toString());
+                    tilePolys[currY - offsetYtile][currX - offsetXtile] = bboxPoly;
                 }
             }
         }
+        console.log(tilePolys);
         return tilePolys;
     }
 
+    public simplifyPolygon(poly: Feature<Polygon>): Feature<Polygon> {
+        const coordinates = (poly.geometry as Polygon).coordinates[0];
+        const newCoordinates: Position[] = [];
+        let prevCoord = coordinates[0];
+        newCoordinates.push(prevCoord);
+        for (let i = 1; i < coordinates.length - 1; i++) {
+            prevCoord = coordinates[i - 1];
+            const nextCoord = coordinates[i + 1];
+            const currCoord = coordinates[i];
+            if (! ((prevCoord[0] === currCoord[0] && nextCoord[0] === currCoord[0]) ||
+                 (prevCoord[1] === currCoord[1] && nextCoord[1] === currCoord[1]))) {
+                newCoordinates.push(currCoord);
+            }
+        }
+        newCoordinates.push(coordinates[coordinates.length - 1]);
+        return polygon([newCoordinates]);
+    }
+
+    // TODO: replace this with turf builtin
     private toRad(num: number) {
         return num * Math.PI / 180;
     }
